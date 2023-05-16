07eaeb4d4f2421b1774cbbb1477a86fce63da899
Tomas Hübner <Titantompa@users.noreply.github.com>
2023-05-16 09:01:31 +0200

    Adjusted for larger digits and modified the display loop a little (#2)
    * Adjusted for larger digits and modified the display loop a little
    
    * Fix to align the code with the bigger segments.
    
    * Added WiFi and calls to get count of games played
    
    * Moved secrets to a separate (and local) config file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <TaskScheduler.h>
 #include <NeoPixelBus.h>
 #include <NeoPixelAnimator.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
+#pragma region WiFi Settings
+
+#define QuoteMacro(x) "#x"
+
+const char * hostname = "GoalsCounter"; 
+const char * soft_ap_ssid = QuoteMacro(SOFT_AP_SSID); 
+const char * soft_ap_pwd = QuoteMacro(SOFT_AP_PWD); 
+const char * wifi_ssid = QuoteMacro(WIFI_SSID); 
+const char * wifi_pwd = QuoteMacro(WIFI_PWD);
+// const char * wifi_ssid = "Goals-Guest"; 
+// const char * wifi_pwd = "Gooooal!"; 
+
+IPAddress soft_ap_address(172, 17, 49, 0);
+IPAddress soft_ap_gateway(172, 17, 49, 1);
+IPAddress soft_ap_mask(255, 255, 255, 240);
+
+#pragma endregion
+
+#define REFRESH_INTERVAL_MS 60000
 #define UPDATE_INTERVAL_MS 1000
-#define PIXEL_COUNT 14
-#define PIXEL_PIN 16
+#define PIXEL_COUNT 28
+#define PIXEL_PIN 33
 
 #define SEGMENTS 7
+#define DIGITS 5
+#define SEGMENT_LEDS 4
 
 const static RgbColor Black(0, 0, 0);
 const static RgbColor White(255, 255, 255);
@@ -14,8 +37,7 @@ const static RgbColor Purple(255, 0, 255);
 const static RgbColor Red(255, 0, 0);
 
 typedef NeoGrbFeature MyPixelColorFeature;
-
-const uint8_t PixelsPerSegment[7] = {2, 2, 2, 2, 2, 2, 2};
+typedef NeoEsp32I2s0Ws2812xMethod MyPixelColorMethod;
 
 const uint8_t SevenSegDigit[10] =
 {
@@ -25,8 +47,10 @@ const uint8_t SevenSegDigit[10] =
   0x3E, 0x7E, 0x13, 0x7F, 0x3F
 };
 
+uint32_t CurrentValue;
+uint32_t Timestamp;
 
-void DisplayNumber(int num, int decimal_place, RgbColor color, NeoPixelBus<MyPixelColorFeature, NeoWs2812Method> &_strip)
+void DisplayNumber(int num, int digit_offset, RgbColor color, NeoPixelBus<MyPixelColorFeature, MyPixelColorMethod> &_strip)
 {
   
   uint8_t bitmask = SevenSegDigit[num];
@@ -35,46 +59,122 @@ void DisplayNumber(int num, int decimal_place, RgbColor color, NeoPixelBus<MyPix
 
   for (uint8_t seg = 0; seg < SEGMENTS; seg++)
   {
+    start_pixel = seg * SEGMENT_LEDS + digit_offset * PIXEL_COUNT;
+    segment_color = ((bitmask>>seg) & 0x01) ? color : Black;
 
-    start_pixel = seg * PixelsPerSegment[seg] + decimal_place * PIXEL_COUNT;
-    segment_color = (bitmask & 0x01) ? color : Black;
-
-    for (uint8_t pixel = start_pixel; pixel < start_pixel + PixelsPerSegment[seg]; pixel++)
+    for (uint8_t pixel = 0; pixel < SEGMENT_LEDS; pixel++)
     {
-      _strip.SetPixelColor(pixel, segment_color);
+      _strip.SetPixelColor(pixel+start_pixel, segment_color);
     }
-    
-    bitmask >>= 1;
   }
-
-  _strip.Show();
-
 }
 
-
 #pragma region TASKS
+
+class PollingTask : public Task
+{
+  private:
+  uint32_t& _value;
+  uint32_t& _timestamp;
+
+  void PollValue()
+  {
+    _timestamp = millis();
+
+    HTTPClient httpClient;
+    httpClient.setTimeout(30000);
+    httpClient.setReuse(false);
+    
+    if(!httpClient.begin("https://tomas-hzrqbqznnq-ez.a.run.app/"))
+    {
+      Serial.println("Failed to begin HTTPClient");
+      return;
+    }
+
+    int code = httpClient.GET();
+
+    if(code != HTTP_CODE_OK)
+    {
+      Serial.println("Failed on response from server: "+String(code));
+      return;
+    }
+
+    auto body = httpClient.getString();
+
+    Serial.println("Response from server: "+body);
+
+    int start = body.indexOf("counter");
+    if(start== -1)
+    {
+      Serial.println("Failed, since body doesn't seem to contain 'counter'");
+      return;
+    }
+    start += 8;
+
+    while( body[start] == ' ' || body[start]==':' && start < body.length())
+      start++;
+
+    if(start == body.length())
+    {
+      Serial.println("Failed, unable to find start of numeric value");
+      return;
+    }
+
+    auto end = start;
+    while(isdigit(body[end]) && end < body.length())
+      end++;
+
+    if(end == body.length())
+    {
+      Serial.println("Failed, reached end of payload looking for end of numeric value");
+      return;
+    }
+
+    auto numstr = body.substring(start,end);
+
+    Serial.println("Found counter value: "+numstr);
+
+    _value = atoi(numstr.c_str());
+
+    httpClient.end();
+
+    return;
+  }
+
+  public:
+  PollingTask(Scheduler &scheduler, uint32_t& value, uint32_t& timestamp)
+      : Task(REFRESH_INTERVAL_MS,
+            TASK_FOREVER,
+            [this]
+            { PollValue(); },
+            &scheduler, true),
+        _value(value),
+        _timestamp(timestamp)
+  {
+  }
+};
 
 class TickCounterClass : public Task
 {
 private:
-  NeoPixelBus<MyPixelColorFeature, NeoWs2812Method> &_strip;
-  uint32_t _counter;
+  NeoPixelBus<MyPixelColorFeature, MyPixelColorMethod> &_strip;
+  uint32_t& _counter;
 
 public:
   void TickCounter()
   {
-    DisplayNumber(_counter,0, Red, _strip);
-
-    _counter++;
-    if(_counter > 9)
+    for(auto i = 0; i < DIGITS; i++)
     {
-      _counter = 0;
+      auto digitValue = (_counter/(int)(pow(10, i)+0.5))%10; // todo - calculate the single digit value
+      DisplayNumber(digitValue, i, Red, _strip);
     }
+
+    _strip.Show();
 
     delay(UPDATE_INTERVAL_MS);
   }
 
-  TickCounterClass(Scheduler &scheduler, NeoPixelBus<MyPixelColorFeature, NeoWs2812Method> &strip)
+  TickCounterClass(Scheduler &scheduler, NeoPixelBus<MyPixelColorFeature, MyPixelColorMethod> &strip, uint32_t& counter)
       : Task(
             TASK_IMMEDIATE,
             TASK_FOREVER,
@@ -82,7 +182,7 @@ public:
             { TickCounter(); },
             &scheduler, true),
         _strip(strip),
-        _counter(0)
+        _counter(counter)
   {
   }
 };
@@ -92,20 +192,67 @@ public:
 #pragma region GLOBALS
 
 /* @brief The neopixel strip */
-NeoPixelBus<MyPixelColorFeature, NeoWs2812Method> PixelStrip(PIXEL_COUNT, PIXEL_PIN);
+NeoPixelBus<MyPixelColorFeature, MyPixelColorMethod> PixelStrip(PIXEL_COUNT*DIGITS, PIXEL_PIN);
 
 /* @brief The task scheduler instance */
 Scheduler TaskScheduler;
 
 /* @brief The CounterTask instance controlling the led strip */
-TickCounterClass CounterTask(TaskScheduler, PixelStrip);
+TickCounterClass CounterTask(TaskScheduler, PixelStrip, CurrentValue);
+
+PollingTask RefreshTask(TaskScheduler, CurrentValue, Timestamp);
 
 #pragma endregion
 
 #pragma region SETUP &LOOP
 
+
+void initWifi()
+{
+  auto apSsid = String(soft_ap_ssid)+String(rand(), 0x16);
+
+  Serial.println("Soft AP SSID: " + apSsid);
+
+  // WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_AP_STA);
+  if (!WiFi.softAPConfig(soft_ap_address, soft_ap_gateway, soft_ap_mask))
+  {
+    Serial.println("WiFi.softAPConfig() failed!");
+  }
+  if (!WiFi.softAP(apSsid.c_str(), soft_ap_pwd))
+  {
+    Serial.println("WiFi.softAP() failed!");
+  }
+  WiFi.begin(wifi_ssid, wifi_pwd);
+
+  Serial.print("Soft AP IP: ");
+  Serial.println(WiFi.softAPIP());
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  digitalWrite(LED_BUILTIN, false);
+
+  Serial.print("Wifi Client IP: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup()
 {
+  Serial.begin(9600);
+
+  while(!Serial)
+    delay;
+
+  initWifi();
+
   PixelStrip.Begin();
   PixelStrip.ClearTo(Black);
   PixelStrip.Show();
